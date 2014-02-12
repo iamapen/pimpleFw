3adbe00583b2b9606c082ed06aac658aa0132508
pen <iamapen@studiopoppy.com>
2014-02-12 14:07:41 +0900

    setting autoload

--- unittest/bootstrap.php
+++ unittest/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 $autoloaderScript = __DIR__ . '/../vendor/autoload.php';
 // $autoloaderScript = 'C:\e43\workspace\utLib\vendor\autoload.php';
-
 $loader = require $autoloaderScript;
-$loader->add('pimpleFw', dirname(__FILE__).'/../src');
+// $loader->add('pimpleFw', dirname(__FILE__).'/../src');
 
